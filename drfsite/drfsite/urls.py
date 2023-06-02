a367af03dce4cfa7ba51221d5c0bbdb26da540f5
Riddhub <unkownrid@gmail.com>
2023-06-02 14:24:54 +0300

    + ViewSets + Routers

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,9 +1,42 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from women.views import WomenAPIView
+from women.views import WomenViewSet
+
+
+# class CustomReadOnlyRouter(routers.SimpleRouter):
+#     """
+#     A router for read-only APIs, which doesn't use trailing slashes.
+#     """
+#     routes = [
+#         routers.Route(
+#             url=r'^{prefix}$',
+#             mapping={'get': 'list'},
+#             name='{basename}-list',
+#             detail=False,
+#             initkwargs={'suffix': 'List'}
+#         ),
+#         routers.Route(
+#             url=r'^{prefix}/{lookup}$',
+#             mapping={'get': 'retrieve'},
+#             name='{basename}-detail',
+#             detail=True,
+#             initkwargs={'suffix': 'Detail'}
+#         ),
+#     ]
+
+# router = routers.SimpleRouter()
+# router = CustomReadOnlyRouter()
+router = routers.DefaultRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/womenlist", WomenAPIView.as_view()),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 ]
+
--- drfsite/women/admin.py
+++ drfsite/women/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Women, Category
 
-admin.site.register(Women)
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+# admin.site.register(Women, WomenAdmin)
 admin.site.register(Category)
